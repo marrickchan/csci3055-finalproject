672585efbbe7cda014a4832970019e62267e2de1
Marrick Chan <Marrick Chan>
2016-04-10 18:57:24 -0400

    Added initial function for Dart, tried compiling in JS

--- main.dart
+++ main.dart
@@ -2,5 +2,9 @@ import 'dart:html';
 
 void main() {
 	// Update search while user is searching in the search field
-	// querySelector('#searchField').onInput.listen()
+	querySelector('#searchField').onInput.listen(updateSearch);
+}
+
+void updateSearch(Event e){
+	querySelector('#searchPreview').text = (e.target as InputElement).value;
 }
