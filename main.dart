d10e51af93dcfb9f44dab5b25e9d7c74abdcc6bf
Marrick Chan <Marrick Chan>
2016-04-14 13:22:42 -0400

    Added search functionality (displays a box when searched)

--- main.dart
+++ main.dart
@@ -2,9 +2,24 @@ import 'dart:html';
 
 void main() {
 	// Update search while user is searching in the search field
-	querySelector('#searchField').onInput.listen(updateSearch);
+	query('#searchArea').hidden = true;
+	querySelector('#searchField').onChange.listen(updateSearch);
+	query('#searchRemove').onClick.listen(hideSearch);
 }
 
 void updateSearch(Event e){
+	// Set Search Results to text entered in search box
+	// Make search area visible before displaying
+	viewSearch(1);
 	querySelector('#searchPreview').text = (e.target as InputElement).value;
+	// Clear search after search is conducted
+	(e.target as InputElement).value = '';
+}
+
+void viewSearch(x){
+	query('#searchArea').hidden = !query('#searchArea').hidden;
+}
+
+void hideSearch(Event e){
+	query('#searchArea').hidden = query('#searchArea').hidden;
 }
